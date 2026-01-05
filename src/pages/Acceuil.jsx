896799cbbfdebb2c6ce0b5ee585f03c02d35fcc2
Borys <b.marester@gmail.com>
2026-01-05 10:50:00 +0100

    fin des squelettes des pages

--- src/pages/Acceuil.jsx
+++ src/pages/Acceuil.jsx
@@ -9,51 +9,55 @@ export default function Acceuil() {
         <div>
             <div class="card text-bg-dark">
                 <img src="hero-bg.jpg" class="card-img" alt="hero image"></img>
-                <div class="card-img-overlay">
-                    <h1 class="card-title text-center">Bonjour, je suis Borys MARESTER</h1>
-                    <p class="card-text text-center">Développeur full stack.</p>
+                <div class="d-flex justify-content-center align-items-center card-img-overlay">
+                    <div >
+                        <h1 class="card-title text-center">Bonjour, je suis Borys MARESTER</h1>
+                        <h2 class="card-text text-center">Développeur full stack.</h2>
+                    </div>
 
                 </div>
-            </div>
-            <div class="row row-cols-1 row-cols-md-3 g-4 m-4">
-                <div class="card text-center" >
-                     <h5 class="card-title">A propos</h5>
-                    <div class="card-body">
-                        <img src="john-doe-about.jpg" class="card-img" alt="john doe profil"></img>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum augue nisl,
-                             In vel metus et lectus egestas laoreet. Pellentesque convallis massa lectus, nec ornare risus euismod non.</p>
+            </div >
+            <div class="container-fluid">
+                <div class="row row-cols-1 row-cols-md-3 g-4 m-4">
+                    <div class="card text-center" >
+                        <h5 class="card-title">A propos</h5>
+                        <div class="card-body">
+                            <img src="john-doe-about.jpg" class="card-img" alt="john doe profil"></img>
+                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum augue nisl,
+                                In vel metus et lectus egestas laoreet. Pellentesque convallis massa lectus, nec ornare risus euismod non.</p>
+                        </div>
                     </div>
-                </div>
-                <div class="col">
-                    <div class="container text-center">
-                        <div class="card" >
-                            <div class="card-body">
-                                <h5 class="card-title">Mes compétences</h5>
+                    <div class="col  ">
+                        <div class="container text-center">
+                            <div class="card" >
+                                <div class="card-body">
+                                    <h5 class="card-title">Mes compétences</h5>
                                     <p>HTML5 90% </p>
                                     <div class="progress">
-                                    <div class="progress-bar bg-danger" role="progressbar" aria-label="Success example" style={{width: "90%"}} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                                        <div class="progress-bar bg-danger" role="progressbar" aria-label="Success example" style={{ width: "90%" }} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
                                     </div>
                                     <p>CSS3 80% </p>
                                     <div class="progress">
-                                    <div class="progress-bar bg-info" role="progressbar" aria-label="Info example" style={{width: "80%"}} aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                                        <div class="progress-bar bg-info" role="progressbar" aria-label="Info example" style={{ width: "80%" }} aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
                                     </div>
                                     <p>JavaScript 70% </p>
                                     <div class="progress">
-                                    <div class="progress-bar bg-warning" role="progressbar" aria-label="Warning example" style={{width: "70%"}} aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                                        <div class="progress-bar bg-warning" role="progressbar" aria-label="Warning example" style={{ width: "70%" }} aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
                                     </div>
                                     <p>PHP 60% </p>
                                     <div class="progress">
-                                    <div class="progress-bar bg-success" role="progressbar" aria-label="Danger example" style={{width: "60%"}} aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+                                        <div class="progress-bar bg-success" role="progressbar" aria-label="Danger example" style={{ width: "60%" }} aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
                                     </div>
                                     <p>React 50% </p>
                                     <div class="progress">
-                                    <div class="progress-bar bg-primary" role="progressbar" aria-label="Danger example" style={{width: "50%"}} aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
-                                    </div>                         
-                                     </div>
+                                        <div class="progress-bar bg-primary" role="progressbar" aria-label="Danger example" style={{ width: "50%" }} aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-     </div>
+            </div>
         </div>
     )
 }
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,17 +1,61 @@
-import { useEffect, useState } from "react";
 
+export default function Contact() {
+    return (
+        <div className="container-fluid">
+            <div className="container text-center">
+                <h1>Contact</h1>
+                <p>Pour me contacter en vue d'un entretien ou d'une future collaboration, merci de remplir le formulaire de contact</p>
+                <div className="d-flex justify-content-around">
+                    <div className="col">
+                        <p className="h2">
+                            Formulaire de contact
+                        </p>
+                        <div className="">
+                            <label for="ControlInput1" className="form-label"></label>
+                            <input type="nom" class="form-control" id="ControlInput1" placeholder="Votre nom" />
+                        </div>
+                        <div className="">
+                            <label for="ControlInput1" className="form-label"></label>
+                            <input type="email" className="form-control" id="ControlInput1" placeholder="votre adresse mail" />
+                        </div>
+                        <div className="">
+                            <label for="ControlInput1" className="form-label"></label>
+                            <input type="tel" class="form-control" id="ControlInput1" placeholder="Votre numéro de téléphone" />
+                        </div>
 
-export default function Contact (){
-    const [count, setCount] = useState(0);
-
-  useEffect(() => {
-      setCount((count) => count + 1);
-  }, []);
-
-
-    return(
-        <div>
-            <h1>{() => 'coucou'}</h1>
+                        <div className="">
+                            <label for="ControlInput1" className="form-label"></label>
+                            <input type="text" className="form-control" id="ControlInput1" placeholder="Sujet" />
+                        </div>
+                        <div className="">
+                            <label for="exampleFormControlTextarea1" className="form-label"></label>
+                            <textarea className="form-control" id="exampleFormControlTextarea1" rows="10" placeholder="Votre message"></textarea>
+                        </div>
+                        <div className="col-center">
+                            <button type="submit" class="btn btn-primary mb-3">ENVOYEZ</button>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div >
+                            <h2>Mes coordonnées</h2>
+                        </div>
+                        <div>
+                            <ul className="listeContact">
+                                <li>40 rue Laure Diebold</li>
+                                <li>69009 Lyon, France</li>
+                                <li>01 20 30 40 50</li>
+                                <li>john.doe@gmail.com</li>
+                            </ul>
+                        </div>
+                        <div>
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2782.6271524621757!2d4.796403976789853!3d45.77866197108087!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47f4eb65edac5b3f%3A0xe01c47049cb2e2b9!2s40%20Rue%20Laure%20Diebold%2C%2069009%20Lyon!5e0!3m2!1sfr!2sfr!4v1767108023028!5m2!1sfr!2sfr" width="600" height="450" style={{ "border": "0" }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
+
+
+
 }
